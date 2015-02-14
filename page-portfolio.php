4b78a74b7a5665af71f2cc0cb778d6f5eb665406
latitudecode <sambakersalmon@gmail.com>
2015-02-14 15:48:55 -0500

    adsf

--- page-portfolio.php
+++ page-portfolio.php
@@ -1,9 +1,82 @@
 <?php
 /*
- * Template Name: Portfolio
+ * Template Name: Work
  * Description: A Page Portfolio page template
  */
 
 //
 
 ?>
+
+
+<?php get_header(); ?>
+	<!-- start main content product -->
+
+	<section class="main-content product-details"> <!-- main product wrapper -->
+		<div class="product-section"> <!-- product wrap -->
+				<div class="row row-effect"> <!-- columns of mediums -->
+
+							<div class="col-md-4">  <!-- medium entry -->
+								<ul class="media">
+									<li>
+										<a href="#">	
+											<figure class="effect-hover"> 
+												<img src="<?php bloginfo('template_directory'); ?>/images/mosaic.png" alt="img15"/> 
+													<figcaption>
+														<p></p>
+													</figcaption>			
+											</figure> 
+										</a>
+									</li>
+									<li>	
+										<h2>mosaic</h2>
+										<p>A series of collage drawings and works that often and integrate bamboo for a three dimensional effect.</p>
+									</li>
+								</ul>
+							</div>	<!-- medium entry -->
+
+							<div class="col-md-4">  <!-- medium entry -->
+								<ul class="media">
+									<li>
+										<a href="#">	
+											<figure class="effect-hover"> 
+												<img src="<?php bloginfo('template_directory'); ?>/images/mosaic.png" alt="img15"/> 
+													<figcaption>
+														<p></p>
+													</figcaption>			
+											</figure> 
+										</a>
+									</li>
+									<li>	
+										<h2>mosaic</h2>
+										<p>A series of collage drawings and works that often and integrate bamboo for a three dimensional effect.</p>
+									</li>
+								</ul>
+							</div>	<!-- medium entry -->
+
+							<div class="col-md-4">  <!-- medium entry -->
+								<ul class="media">
+									<li>
+										<a href="#">	
+											<figure class="effect-hover"> 
+												<img src="<?php bloginfo('template_directory'); ?>/images/mosaic.png" alt="img15"/> 
+													<figcaption>
+														<p></p>
+													</figcaption>			
+											</figure> 
+										</a>
+									</li>
+									<li>	
+										<h2>mosaic</h2>
+										<p>A series of collage drawings and works that often and integrate bamboo for a three dimensional effect.</p>
+									</li>
+								</ul>
+							</div>	<!-- medium entry -->
+
+				</div> <!-- end columns of mediums -->
+		</div> <!-- end product wrap -->
+	</section> <!-- end main product wrapper -->
+
+	<!-- end main content product -->
+<?php
+get_footer();
