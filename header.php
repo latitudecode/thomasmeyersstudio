08cc28777964a9880625cb106fbf5d7c42b53d7f
latitudecode <sambakersalmon@gmail.com>
2015-02-14 14:20:08 -0500

    adding head wp tags

--- header.php
+++ header.php
@@ -1,1 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width" />
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/folder/file-name.js"></script>
+
 <?php wp_head(); ?>
+</head>
