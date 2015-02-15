730eb6273dd336b25eaeed8bac008a986ff88021
latitudecode <sambakersalmon@gmail.com>
2015-02-14 21:07:57 -0500

    making homepage dynamic

--- index.php
+++ index.php
@@ -26,37 +26,54 @@
 	<!-- start the medium seleciton with secondary nav -->
 	
 	<section class="cat-section row-effect">
-		<div class="media-entry-mosaic fl"> <!-- nav for selecting medium type -->
-			 <ul>
-				<li class="title"><h2>mosaic</h2></li>
-				<li class="cat-img">
-					<a href="#">	
-						<figure class="effect-hover cat"> 
-							<img src="<?php bloginfo('template_directory'); ?>/images/mosaic.png" />
-								<figcaption>
-									<p></p>
-								</figcaption>			
-						</figure> 
-					</a>					
-				</li>
-			</ul>
-		</div> <!-- end nav for selecting type -->
-		
-		<div class="media-entry-collage fr"> <!-- nav for selecting medium type -->
-			 <ul>
-				<li class="title"><h2>collage</h2></li>
-				<li class="cat-img">						
-					<a href="#">	
-						<figure class="effect-hover cat"> 
-							<img src="<?php bloginfo('template_directory'); ?>/images/mosaic.png" />
-								<figcaption>
-									<p></p>
-								</figcaption>			
-						</figure> 
-					</a>
-				</li>
-			</ul>
-		</div> <!-- end nav for selecting type -->
+		<?php query_posts('page_id=16'); if(have_posts()) : while(have_posts()) : the_post(); ?><!-- starting the WordPress loop -->
+
+			<div class="media-entry-mosaic fl"> <!-- nav for selecting medium type -->
+				 <ul>
+					<li class="title"><h2>mosaic</h2></li>
+					<li class="cat-img">
+						<a href="<?php the_permalink(); ?>">	
+							<figure class="effect-hover cat"> 
+					          <?php 
+	                            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+	                              the_post_thumbnail();
+	                            } 
+	                            ?>				
+							</figure> 
+						</a>					
+					</li>
+				</ul>
+			</div> <!-- end nav for selecting type -->
+
+		<?php endwhile; ?>
+		<?php else : ?>
+		    <p>Whoops! We are working on it.</p>
+		<?php endif; ?> <!-- end loop -->
+
+		<?php query_posts('page_id=18'); if(have_posts()) : while(have_posts()) : the_post(); ?><!-- starting the WordPress loop -->	
+
+			<div class="media-entry-collage fr"> <!-- nav for selecting medium type -->
+				 <ul>
+					<li class="title"><h2>collage</h2></li>
+					<li class="cat-img">						
+						<a href="<?php the_permalink(); ?>">	
+							<figure class="effect-hover cat"> 
+					          <?php 
+	                            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+	                              the_post_thumbnail();
+	                            } 
+	                            ?>				
+							</figure> 
+						</a>
+					</li>
+				</ul>
+			</div> <!-- end nav for selecting type -->
+
+		<?php endwhile; ?>
+		<?php else : ?>
+		    <p>Whoops! We are working on it.</p>
+		<?php endif; ?> <!-- end loop -->
+
 	</section>
 	<div class="clear"></div>
 
