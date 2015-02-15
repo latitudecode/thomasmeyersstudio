6f2f774afbe60ede4e598b5c3893a61d2fb0cc56
latitudecode <sambakersalmon@gmail.com>
2015-02-14 21:31:17 -0500

    adsf

--- footer.php
+++ footer.php
@@ -16,9 +16,9 @@
 			</ul>
 		</div> <!-- end product menu -->		
 		<div class="signup-man fl">
-			<input class="signup-field" type="email" required="" placeholder="Email address" id="mce-EMAIL" name="EMAIL" value="">
+			<input class="signup-field" type="email" required="" placeholder="Signup for updates" id="mce-EMAIL" name="EMAIL" value="">
 				<button class="signup-button" id="mc-embedded-subscribe" name="subscribe" type="submit">
-	            	<i class="#"></i>
+	            	<a href="#"><i class="fa fa-angle-right"></i></a>
 	            </button>
         </div>
 		<div class="rightz fr"><h4>&copy; 2014 Thomas Meyers</h4></div>
