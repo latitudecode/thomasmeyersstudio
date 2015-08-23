97bf3779240d5a36802374b82e4b5bc2627fbfd2
latitudecode <sambakersalmon@gmail.com>
2015-08-23 17:46:00 -0400

    custom post meta

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 add_theme_support( 'post-thumbnails' ); 
 
+
 /*-----------------------------------------------------------------------------------*/
 /* Custom post types*/
 /*-----------------------------------------------------------------------------------*/
@@ -94,45 +95,70 @@ function creater_post_typer() {
 
 /* GALLERY META BOX */
 
+/* add the action */
 add_action( 'add_meta_boxes', 'cd_meta_box_add' );
 
+/* creates the meta box in backend */
 function cd_meta_box_add()
 {
-    add_meta_box( 'my-meta-box-id', 'Gallery', 'cd_meta_box_cb', 'galleries', 'normal', 'high' );
+    add_meta_box( 'my-meta-box-id', 'My First Meta Box', 'cd_meta_box_cb', 'post', 'normal', 'high' );
 }
 
+/* creates the form fields in backend */
 function cd_meta_box_cb()
 {
-{
+
     // $post is already set, and contains an object: the WordPress post
     global $post;
     $values = get_post_custom( $post->ID );
     $text = isset( $values['my_meta_box_text'] ) ? $values['my_meta_box_text'] : '';
-    $text = isset( $values['my_meta_box_text2'] ) ? $values['my_meta_box_text2'] : '';
-    $text = isset( $values['my_meta_box_text3'] ) ? $values['my_meta_box_text3'] : '';
-
      
     // We'll use this nonce field later on when saving.
     wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
     ?>
     <p>
-        <label for="my_meta_box_text">City</label>
+        <label for="my_meta_box_text">Text Label</label>
         <input type="text" name="my_meta_box_text" id="my_meta_box_text" value="<?php echo $text; ?>" />
     </p>
-
-    <p>
-        <label for="my_meta_box_text">State</label>
-        <input type="text" name="my_meta_box_text2" id="my_meta_box_text2" value="<?php echo $text; ?>" />
-    </p>
-
-    <p>
-        <label for="my_meta_box_text">Website</label>
-        <input type="text" name="my_meta_box_text3" id="my_meta_box_text3" value="<?php echo $text; ?>" />
-    </p>         
-
+     
     <?php    
-}  
+} 
+
+
+
+/* saves post */
+add_action( 'save_post', 'cd_meta_box_save' );
+function cd_meta_box_save( $post_id )
+{
+    // Bail if we're doing an auto save
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+     
+    // if our nonce isn't there, or we can't verify it, bail
+    if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return;
+     
+    // if our current user can't edit this post, bail
+    if( !current_user_can( 'edit_post' ) ) return;
+     
+    // now we can actually save the data
+    $allowed = array( 
+        'a' => array( // on allow a tags
+            'href' => array() // and those anchors can only have href attribute
+        )
+    );
+     
+    // Make sure your data is set before trying to save it
+    if( isset( $_POST['my_meta_box_text'] ) )
+        update_post_meta( $post_id, 'my_meta_box_text', wp_kses( $_POST['my_meta_box_text'], $allowed ) );
+         
+    if( isset( $_POST['my_meta_box_select'] ) )
+        update_post_meta( $post_id, 'my_meta_box_select', esc_attr( $_POST['my_meta_box_select'] ) );
+         
+    // This is purely my personal preference for saving check-boxes
+    $chk = isset( $_POST['my_meta_box_check'] ) && $_POST['my_meta_box_select'] ? 'on' : 'off';
+    update_post_meta( $post_id, 'my_meta_box_check', $chk );
 }
+
+
 
 
 /*-----------------------------------------------------------------------------------*/
